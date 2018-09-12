b51bde85986150dfc3b6a98985334eb9b55fd1dc
Răzvan Tudorică <razvantudorica@gmail.com>
2018-09-12 18:30:00 +0300

    change install script

--- contrib/openvpn-install.sh
+++ contrib/openvpn-install.sh
@@ -225,14 +225,18 @@ else
 		yum install openvpn iptables openssl ca-certificates -y
 	fi
 	# Get easy-rsa
-	EASYRSAURL='https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.4/EasyRSA-3.0.4.tgz'
-	wget -O ~/easyrsa.tgz "$EASYRSAURL" 2>/dev/null || curl -Lo ~/easyrsa.tgz "$EASYRSAURL"
-	tar xzf ~/easyrsa.tgz -C ~/
-	mv ~/EasyRSA-3.0.4/ /etc/openvpn/
-	mv /etc/openvpn/EasyRSA-3.0.4/ /etc/openvpn/easy-rsa/
-	chown -R root:root /etc/openvpn/easy-rsa/
-	rm -f ~/easyrsa.tgz
+	# EASYRSAURL='https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.4/EasyRSA-3.0.4.tgz'
+	# wget -O ~/easyrsa.tgz "$EASYRSAURL" 2>/dev/null || curl -Lo ~/easyrsa.tgz "$EASYRSAURL"
+	# tar xzf ~/easyrsa.tgz -C ~/
+	# mv ~/EasyRSA-3.0.4/ /etc/openvpn/
+	# mv /etc/openvpn/EasyRSA-3.0.4/ /etc/openvpn/easy-rsa/
+	# chown -R root:root /etc/openvpn/easy-rsa/
+	# rm -f ~/easyrsa.tgz
+	# cd /etc/openvpn/easy-rsa/
+	mkdir /etc/openvpn/easy-rsa/
+	cp -r ../easyrsa3/* /etc/openvpn/easy-rsa/
 	cd /etc/openvpn/easy-rsa/
+
 	# Create the PKI, set up the CA, the DH params and the server + client certificates
 	./easyrsa init-pki
 	./easyrsa --batch build-ca nopass
