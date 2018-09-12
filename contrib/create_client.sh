450e148052e7ee5bd20c020a4516085ebcb9411f
Răzvan Tudorică <razvantudorica@gmail.com>
2018-09-12 17:38:45 +0300

    support to send the password as argument when creating full client cert; wrapper script to create user

---
+++ contrib/create_client.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 USERNAME PASSWORD"
+    exit 1
+fi
+
+newclient () {
+	cd /etc/openvpn/easy-rsa/
+    ./easyrsa build-client-full $1 pass:$2 # nopass
+	# Generates the custom client.ovpn
+	cp /etc/openvpn/client-common.txt ~/$1.ovpn
+	echo "<ca>" >> ~/$1.ovpn
+	cat /etc/openvpn/easy-rsa/pki/ca.crt >> ~/$1.ovpn
+	echo "</ca>" >> ~/$1.ovpn
+	echo "<cert>" >> ~/$1.ovpn
+	cat /etc/openvpn/easy-rsa/pki/issued/$1.crt >> ~/$1.ovpn
+	echo "</cert>" >> ~/$1.ovpn
+	echo "<key>" >> ~/$1.ovpn
+	cat /etc/openvpn/easy-rsa/pki/private/$1.key >> ~/$1.ovpn
+	echo "</key>" >> ~/$1.ovpn
+	echo "<tls-auth>" >> ~/$1.ovpn
+	cat /etc/openvpn/ta.key >> ~/$1.ovpn
+	echo "</tls-auth>" >> ~/$1.ovpn
+}
+
+newclient $1 $2
+
