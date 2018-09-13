2b3ef3f38c619c60448317d3eaf3d1d047e46357
Răzvan Tudorică <razvantudorica@gmail.com>
2018-09-13 12:59:03 +0300

    change create client

--- contrib/create_client.sh
+++ contrib/create_client.sh
@@ -4,23 +4,26 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
+CERTS="/etc/openvpn/client-certs"
+mkdir -p $CERTS
+
 newclient () {
 	cd /etc/openvpn/easy-rsa/
     ./easyrsa build-client-full $1 pass:$2 # nopass
 	# Generates the custom client.ovpn
-	cp /etc/openvpn/client-common.txt ~/$1.ovpn
-	echo "<ca>" >> ~/$1.ovpn
-	cat /etc/openvpn/easy-rsa/pki/ca.crt >> ~/$1.ovpn
-	echo "</ca>" >> ~/$1.ovpn
-	echo "<cert>" >> ~/$1.ovpn
-	cat /etc/openvpn/easy-rsa/pki/issued/$1.crt >> ~/$1.ovpn
-	echo "</cert>" >> ~/$1.ovpn
-	echo "<key>" >> ~/$1.ovpn
-	cat /etc/openvpn/easy-rsa/pki/private/$1.key >> ~/$1.ovpn
-	echo "</key>" >> ~/$1.ovpn
-	echo "<tls-auth>" >> ~/$1.ovpn
-	cat /etc/openvpn/ta.key >> ~/$1.ovpn
-	echo "</tls-auth>" >> ~/$1.ovpn
+	cp /etc/openvpn/client-common.txt $CERTS/$1.ovpn
+	echo "<ca>" >> $CERTS/$1.ovpn
+	cat /etc/openvpn/easy-rsa/pki/ca.crt >> $CERTS/$1.ovpn
+	echo "</ca>" >> $CERTS/$1.ovpn
+	echo "<cert>" >> $CERTS/$1.ovpn
+	cat /etc/openvpn/easy-rsa/pki/issued/$1.crt >> $CERTS/$1.ovpn
+	echo "</cert>" >> $CERTS/$1.ovpn
+	echo "<key>" >> $CERTS/$1.ovpn
+	cat /etc/openvpn/easy-rsa/pki/private/$1.key >> $CERTS/$1.ovpn
+	echo "</key>" >> $CERTS/$1.ovpn
+	echo "<tls-auth>" >> $CERTS/$1.ovpn
+	cat /etc/openvpn/ta.key >> $CERTS/$1.ovpn
+	echo "</tls-auth>" >> $CERTS/$1.ovpn
 }
 
 newclient $1 $2
